2782c74874f97666a97d649ca4acd4a058335f28
Andreas Holmberg <andreas@burken.se>
2026-09-19 14:55:34 +0200

    feat: warn on entities for unknown registers

--- components/nibe/sensor.py
+++ components/nibe/sensor.py
@@ -1,9 +1,12 @@
-# components/nibe/sensor.py
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
+from esphome.core import EsphomeError
+import logging, os, json
 from . import Nibe, nibe_ns
+from .registers import is_known
 
+_LOGGER = logging.getLogger(__name__)
 NibeSensor = nibe_ns.class_("NibeSensor", sensor.Sensor, cg.Component)
 
 CONFIG_SCHEMA = sensor.sensor_schema(NibeSensor).extend({
@@ -11,8 +14,15 @@ CONFIG_SCHEMA = sensor.sensor_schema(NibeSensor).extend({
     cv.Required("register"): cv.int_,
 })
 
-
 async def to_code(config):
+    mdir = os.path.join(os.path.dirname(__file__), "models")
+    models = {}
+    for f in sorted(os.listdir(mdir)):
+        if f.endswith(".json"):
+            with open(os.path.join(mdir, f)) as fh:
+                models[f[:-5]] = json.load(fh)
+    if not is_known(config["register"], models):
+        _LOGGER.warning("nibe sensor register %s not in model maps; entity will never fire", config["register"])
     parent = await cg.get_variable(config["nibe_id"])
     var = await sensor.new_sensor(config)
     await cg.register_component(var, config)
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,6 @@
 import sys
 from unittest.mock import MagicMock
 
-for _name in ("esphome", "esphome.codegen", "esphome.config_validation",
+for _name in ("esphome", "esphome.codegen", "esphome.config_validation", "esphome.core",
               "esphome.components", "esphome.components.uart"):
     sys.modules.setdefault(_name, MagicMock(name=_name))
